a2ae0a76444ac8110097f72bcc31f4089e5be60e
spyophobia <76800505+spyophobia@users.noreply.github.com>
2021-10-22 01:12:42 +0800

    Minor imprv

--- run-local.sh
+++ run-local.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/sh
+set -e # exit on error
 
 cd $(dirname "$0") # enter project root dir
-mkdir "./local-run"
+mkdir -p "./local-run" # create temp directory
+
 cargo run --release -- \
   -v \
   --profiles-dir "./example-config-profiles" \

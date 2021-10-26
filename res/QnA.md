b0bba925a34bcab28ab5e2f31caa01a758c88dab
spyophobia <76800505+spyophobia@users.noreply.github.com>
2021-10-26 09:33:52 +0800

    Infrastructure for CLI arg profile

--- res/QnA.md
+++ res/QnA.md
@@ -12,7 +12,7 @@ We will call this a "profile directory". You can name this directory anything yo
 
 `profile.yaml` is the definition file for a profile: a directory is considered a "profile directory" only if it contains a `profile.yaml`. You may put other profile-specific files and directories under this directory too if you need to reference them from `profile.yaml`, such as a `ss-config.json`.
 
-For an example of how to write a `profile.yaml`, see [example-config-profiles/A-good-profile](example-config-profiles/A-good-profile).
+For examples of how to manually write a `profile.yaml`, see [example-config-profiles/A-good-profile-using-cli-args](example-config-profiles/A-good-profile-using-cli-args) and [example-config-profiles/A-good-profile-using-config-file](example-config-profiles/A-good-profile-using-config-file).
 ### Grouped multi-profile config
 ```
 # An example
--- src/io/config_loader.rs
+++ src/io/config_loader.rs
@@ -5,6 +5,7 @@ use std::{
     fmt::Display,
     fs::read_to_string,
     io,
+    net::{IpAddr, Ipv6Addr},
     path::{Path, PathBuf},
     process::{Child, Command, Stdio},
 };
@@ -19,12 +20,18 @@ const SSLOCAL_DEFAULT_PATH: &str = "sslocal";
 const PROFILE_DEF_FILE_NAME: &str = "profile.yaml";
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ConfigProfileSerde {
-    pub display_name: Option<String>,
-    pub pwd: Option<PathBuf>,
-    pub ss_bin_path: Option<PathBuf>,
-    pub ss_config_path: Option<PathBuf>,
-    pub extra_args: Option<Vec<String>>,
+struct ConfigProfileSerde {
+    display_name: Option<String>,
+    pwd: Option<PathBuf>,
+    bin_path: Option<PathBuf>,
+
+    local_addr: Option<(IpAddr, u16)>,
+    server_addr: Option<(String, u16)>,
+    password: Option<String>,
+    encrypt_method: Option<String>,
+
+    config_path: Option<PathBuf>,
+    extra_args: Option<Vec<String>>,
 }
 
 impl TryInto<ConfigProfile> for ConfigProfileSerde {
@@ -34,20 +41,28 @@ impl TryInto<ConfigProfile> for ConfigProfileSerde {
         let Self {
             display_name,
             pwd,
-            ss_bin_path,
-            ss_config_path,
+            bin_path,
+            local_addr,
+            server_addr,
+            password,
+            encrypt_method,
+            config_path,
             extra_args,
         } = self;
 
         let display_name = display_name.ok_or("display_name not set".to_string())?;
         let pwd = pwd.ok_or("pwd not set".to_string())?;
-        let ss_bin_path = ss_bin_path.ok_or("ss_bin_path not set".to_string())?;
+        let bin_path = bin_path.ok_or("ss_bin_path not set".to_string())?;
 
         Ok(ConfigProfile {
             display_name,
             pwd,
-            ss_bin_path,
-            ss_config_path,
+            bin_path,
+            local_addr,
+            server_addr,
+            password,
+            encrypt_method,
+            config_path,
             extra_args,
         })
     }
@@ -55,11 +70,20 @@ impl TryInto<ConfigProfile> for ConfigProfileSerde {
 
 #[derive(Debug, Clone)]
 pub struct ConfigProfile {
+    // mandatory fields
     pub display_name: String,
-    pub pwd: PathBuf,
-    pub ss_bin_path: PathBuf,
-    pub ss_config_path: Option<PathBuf>,
-    pub extra_args: Option<Vec<String>>,
+    pwd: PathBuf,
+    bin_path: PathBuf,
+
+    // simple config fields
+    local_addr: Option<(IpAddr, u16)>,
+    server_addr: Option<(String, u16)>,
+    password: Option<String>,
+    encrypt_method: Option<String>,
+
+    // advanced config fields
+    config_path: Option<PathBuf>,
+    extra_args: Option<Vec<String>>,
 }
 
 impl ConfigProfile {
@@ -72,17 +96,43 @@ impl ConfigProfile {
         O: Into<Stdio>,
         E: Into<Stdio>,
     {
-        let config_args: Vec<OsString> = self
-            .ss_config_path
+        let local_addr_args = self.local_addr.as_ref().map_or(vec![], |(a, p)| {
+            let addr = match a {
+                IpAddr::V4(v4) => format!("{}:{}", v4, p),
+                IpAddr::V6(v6) => format!("[{}]:{}", v6, p),
+            };
+            vec!["--local-addr".into(), addr]
+        });
+        let server_addr_args = self.server_addr.as_ref().map_or(vec![], |(a, p)| {
+            let addr = match a.parse::<Ipv6Addr>() {
+                Ok(_) => format!("[{}]:{}", a, p), // IPv6
+                Err(_) => format!("{}:{}", a, p),  // Domain or IPv4
+            };
+            vec!["--server-addr".into(), addr]
+        });
+        let password_args = self
+            .password
+            .as_ref()
+            .map_or(vec![], |pass| vec!["--password".into(), pass.clone()]);
+        let encrypt_method_args = self
+            .encrypt_method
+            .as_ref()
+            .map_or(vec![], |m| vec!["--encrypt-method".into(), m.clone()]);
+        let config_path_args: Vec<OsString> = self
+            .config_path
             .as_ref()
             .map_or(vec![], |p| vec!["--config".into(), p.into()]);
         let extra_args = self.extra_args.clone().unwrap_or(Vec::new()); // better would be to return a slice but I can't be arsed
         let stdout = stdout.map_or(Stdio::null(), |o| o.into());
         let stderr = stderr.map_or(Stdio::null(), |e| e.into());
 
-        Command::new(self.ss_bin_path.clone())
+        Command::new(self.bin_path.clone())
             .current_dir(self.pwd.clone())
-            .args(config_args)
+            .args(local_addr_args)
+            .args(server_addr_args)
+            .args(password_args)
+            .args(encrypt_method_args)
+            .args(config_path_args)
             .args(extra_args)
             .stdin(Stdio::null()) // sslocal does not read from stdin
             .stdout(stdout)
@@ -192,7 +242,7 @@ impl ConfigFolder {
                 },
             ));
             // set default binary path
-            profile.ss_bin_path.get_or_insert(SSLOCAL_DEFAULT_PATH.into());
+            profile.bin_path.get_or_insert(SSLOCAL_DEFAULT_PATH.into());
             return Ok(Self::Profile(
                 profile.try_into().unwrap(), // required fields are set
             ));
